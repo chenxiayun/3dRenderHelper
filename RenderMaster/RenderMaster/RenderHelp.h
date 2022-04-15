c2c7792bd3d8c016ae1aedd2079ee5ee27ee01a8
陈子剑 <chenzijian@hello-alpha.com>
2022-04-15 20:21:31 +0800

    【开发】 BMP储存代码copy

--- RenderMaster/RenderMaster/RenderHelp.h
+++ RenderMaster/RenderMaster/RenderHelp.h
@@ -338,6 +338,11 @@ public:
 public:
 	inline int GetW() const { return _w; }
 	inline int GetH() const { return _h; }
+	inline int GetPitch() const { return _pitch; }
+	inline uint8_t *GetBits() { return _bits; }
+	inline const uint8_t *GetBits() const { return _bits; }
+	inline uint8_t *GetLine(int y) { return _bits + _pitch * y; }
+	inline const uint8_t *GetLine(int y) const { return _bits + _pitch * y; }
 
 public:
 	
@@ -451,13 +456,42 @@ public:
 	};
 
 	// ±£´æ BMP ÎÄ¼þ
-	inline bool SaveFile(const char *filename, bool withAlpha = false) const 
-	{
+	inline bool SaveFile(const char *filename, bool withAlpha = false) const {
 		FILE *fp = fopen(filename, "wb");
 		if (fp == NULL) return false;
-
 		BITMAPINFOHEADER info;
-
+		uint32_t pixelsize = (withAlpha) ? 4 : 3;
+		uint32_t pitch = (GetW() * pixelsize + 3) & (~3);
+		info.biSizeImage = pitch * GetH();
+		uint32_t bfSize = 54 + info.biSizeImage;
+		uint32_t zero = 0, offset = 54;
+		fputc(0x42, fp);
+		fputc(0x4d, fp);
+		fwrite(&bfSize, 4, 1, fp);
+		fwrite(&zero, 4, 1, fp);
+		fwrite(&offset, 4, 1, fp);
+		info.biSize = 40;
+		info.biWidth = GetW();
+		info.biHeight = GetH();
+		info.biPlanes = 1;
+		info.biBitCount = (withAlpha) ? 32 : 24;
+		info.biCompression = 0;
+		info.biXPelsPerMeter = 0xb12;
+		info.biYPelsPerMeter = 0xb12;
+		info.biClrUsed = 0;
+		info.biClrImportant = 0;
+		fwrite(&info, sizeof(info), 1, fp);
+		// printf("pitch=%d %d\n", (int)pitch, info.biSizeImage);
+		for (int y = 0; y < GetH(); y++) {
+			const uint8_t *line = GetLine(info.biHeight - 1 - y);
+			uint32_t padding = pitch - GetW() * pixelsize;
+			for (int x = 0; x < GetW(); x++, line += 4) {
+				fwrite(line, pixelsize, 1, fp);
+			}
+			for (int i = 0; i < (int)padding; i++) fputc(0, fp);
+		}
+		fclose(fp);
+		return true;
 	}
 
 protected:
